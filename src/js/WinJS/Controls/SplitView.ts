37a2dfb2e805af51b3eb321c4d5971b9c5d77c4a
Adam Comella <adcom@microsoft.com>
2014-10-13 17:48:19 -0700

    Adjust indentation

--- src/js/WinJS/Controls/SplitView.ts
+++ src/js/WinJS/Controls/SplitView.ts
@@ -3,13 +3,13 @@ import _SplitView = require('./SplitView/_SplitView');
 declare var require: any;
 
 _Base.Namespace.define("WinJS.UI", {
-	SplitView: {
-		get: () => {
-			var module: typeof _SplitView = null;
-			require(["./SplitView/_SplitView"], (m: typeof _SplitView) => {
-	        	module = m;
-			});
-			return module;
-		}
+    SplitView: {
+        get: () => {
+            var module: typeof _SplitView = null;
+            require(["./SplitView/_SplitView"], (m: typeof _SplitView) => {
+                module = m;
+            });
+            return module;
+        }
     }
 });
